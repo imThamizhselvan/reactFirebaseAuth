ceb59eb10bf8d210531e384f11bf8f99abbee953
Thamizhselvan <thamizhtechie@gmail.com>
2018-10-08 08:08:01 +0530

    build

---
+++ build/precache-manifest.f4eceb954908ee8437a4448fce11ea77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "954e4f5d18db65abc381",
+    "url": "/static/js/main.954e4f5d.chunk.js"
+  },
+  {
+    "revision": "fcfe627f6d3b8dbe05a2",
+    "url": "/static/js/1.fcfe627f.chunk.js"
+  },
+  {
+    "revision": "954e4f5d18db65abc381",
+    "url": "/static/css/main.f52a70c4.chunk.css"
+  },
+  {
+    "revision": "058e89a83ad233d7b25e2ba784226628",
+    "url": "/index.html"
+  }
+];
